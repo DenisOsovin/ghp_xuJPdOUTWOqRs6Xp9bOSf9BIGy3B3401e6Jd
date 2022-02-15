62bb30ca20ad6833e56aa20a9b93f37ac3ddc048
DenisOsovin <denisosovin@mail.ru>
2022-02-15 18:43:48 +0300

    добавлено нажатие кнопок, картинку переместил вниз, добавил поле для вывода результата, не пониманию , как получить конечную функцию

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,81 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //вводим объекты для дальнейшей привязки
+    private Button mB1;
+    private Button mB2;
+    private Button mB3;
+    private Button mB4;
+    private Button mB5;
+    private Button mB6;
+    private Button mB7;
+    private Button mB8;
+    private Button mB9;
+    private Button mB10;
+    private Button mB0;
+    private Button mBPoint;
+    private Button mBPlus;
+    private Button mBMinus;
+    private Button mBMultiplication;
+    private Button mBDivision;
+    private Button mBEqually;
+    private TextView mResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //связываем объеты с элементами в layout
+        mB1 = (Button) findViewById(R.id.button_1);
+        mB2 = (Button) findViewById(R.id.button_2);
+        mB3 = (Button) findViewById(R.id.button_3);
+        mB4 = (Button) findViewById(R.id.button_4);
+        mB5 = (Button) findViewById(R.id.button_5);
+        mB6 = (Button) findViewById(R.id.button_6);
+        mB7 = (Button) findViewById(R.id.button_7);
+        mB8 = (Button) findViewById(R.id.button_8);
+        mB9 = (Button) findViewById(R.id.button_9);
+        mB0 = (Button) findViewById(R.id.button_0);
+        mBPoint = (Button) findViewById(R.id.button_point);
+        mBPlus = (Button) findViewById(R.id.button_plus);
+        mBMinus = (Button) findViewById(R.id.button_minus);
+        mBMultiplication = (Button) findViewById(R.id.button_multiplication);
+        mBDivision = (Button) findViewById(R.id.button_division);
+        mBEqually = (Button) findViewById(R.id.button_equally);
+        mResult = (TextView) findViewById(R.id.textView1);
+
+        //добавляем нажатие всех кнопок
+
+        mB1.setOnClickListener(this);
+        mB2.setOnClickListener(this);
+        mB3.setOnClickListener(this);
+        mB4.setOnClickListener(this);
+        mB5.setOnClickListener(this);
+        mB6.setOnClickListener(this);
+        mB7.setOnClickListener(this);
+        mB8.setOnClickListener(this);
+        mB9.setOnClickListener(this);
+        mB0.setOnClickListener(this);
+        mBPoint.setOnClickListener(this);
+        mBPlus.setOnClickListener(this);
+        mBMultiplication.setOnClickListener(this);
+        mBDivision.setOnClickListener(this);
+        mBEqually.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //не знаю, как получить значение в result
+
+        mResult.setText("Вы получили число...");
     }
 }
