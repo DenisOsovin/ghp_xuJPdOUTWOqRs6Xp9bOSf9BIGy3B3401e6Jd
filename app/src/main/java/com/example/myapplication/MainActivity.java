785cf21543108a65f2ca36cec1ef20f063b30594
DenisOsovin <denisosovin@mail.ru>
2022-02-22 17:25:20 +0300

    добавлена тема в манифест, в activity_main, в layout, наслелдование в стили

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String Count ="";
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
